3f973925759a8b83ef6a16cad2d51b5cfdb97155
wanjun <wanjuncode@163.com>
2018-06-01 13:05:11 +0800

    test

--- 第四章/对日志进行聚合操作.py
+++ 第四章/对日志进行聚合操作.py
@@ -7,13 +7,13 @@ callback 用于处理每行操作的回调函数
 """
 
 def process_logs(conn,path,callback):
-    # 获取文件当前的处理进度
+    # 获取文件当前的处理进度 string
     current_file,offset=conn.mget('progress:file','progress:position')
     pipe = conn.pipeline()
 
     # 使用闭包 来减少重复代码
     def update_progress():
-        # 更新正在处理的日志文件的名字和偏移量
+        # 更新正在处理的日志文件的名字和偏移量 string
         pipe.mset({
             'progress:file':fname,
             'progress:position':offset
@@ -35,6 +35,7 @@ def process_logs(conn,path,callback):
         current_file=None
 
         # 枚举函数遍历一个由文件行组成的序列  行号lno  行数据line
+        # enumerate() 函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列，同时列出数据和数据下标，一般用在 for 循环当中。
         for lno,line in enumerate(inp):
             # 使用回调函数处理日志行
             callback(pipe,line)
